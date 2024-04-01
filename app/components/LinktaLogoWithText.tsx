2892d17ce70e1fb99cff945abb5c83896a09e63b
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-01 15:23:38 -0500

    add linkta logo with text component

---
+++ app/components/LinktaLogoWithText.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const LinktaLogoWithText: React.FC = () => {
+  return (
+    <figure>
+      <img
+        src="../public/linkta-logo-gray.svg"
+        width="50"
+        height="50"
+        alt="Linkta Logo"
+      />
+      <figcaption>Linkta</figcaption>
+    </figure>
+  );
+};
+
+export default LinktaLogoWithText;

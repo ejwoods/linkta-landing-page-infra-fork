a9c9c3721e838bb2f0fdeb2af0240da8d4e1619a
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-01 15:26:00 -0500

    replace img with next/image

--- app/components/LinktaLogoWithText.tsx
+++ app/components/LinktaLogoWithText.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Image from 'next/image';
 
 const LinktaLogoWithText: React.FC = () => {
   return (
     <figure>
-      <img
-        src="../public/linkta-logo-gray.svg"
-        width="50"
-        height="50"
+      <Image
+        src="/linkta-logo-gray.svg"
+        width={50}
+        height={50}
         alt="Linkta Logo"
       />
       <figcaption>Linkta</figcaption>

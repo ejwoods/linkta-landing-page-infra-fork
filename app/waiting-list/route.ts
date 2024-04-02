1063a00e79416e7c1c0654772e2bdb2bd7434366
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-02 08:38:26 -0500

    fix typo

--- app/waiting-list/route.ts
+++ app/waiting-list/route.ts
@@ -2,5 +2,5 @@
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  return NextResponse.json({ message: 'Welcome to Linka!🌳' }, { status: 200 })
+  return NextResponse.json({ message: 'Welcome to Linkta!🌳' }, { status: 200 })
 };

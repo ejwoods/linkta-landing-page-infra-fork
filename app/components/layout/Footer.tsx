56df28b11ebb2a4478e20b4492f2b39c0fa2e11c
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-02 09:28:09 -0500

    implement footer

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,5 +1,18 @@
+import { footerLinks } from "./layoutConfig";
+import Link from 'next/link';
+
 export default function Footer (){
   return (
-    <div>Footer</div>
+    <>
+      <div>©2024 Linkta L.L.C. All rights reserved.</div>
+
+      <ul>
+        {footerLinks.map((tab) => (
+          <li key={`${tab.path}-${tab.tabname}`}>
+            <Link href={tab.path}>{tab.tabname}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
---
+++ app/components/layout/layoutConfig.ts
@@ -0,0 +1,24 @@
+export interface Tab {
+  tabname: string;
+  path: string;
+}
+/**
+ * Tab objects representing different routes.
+ */
+//TODO: paths to be updated
+const cookiePreferencesPageRouteTab: Tab = {
+  tabname: 'Cookie Preferences',
+  path: '/cookie_preferences',
+};
+const securityPageRouteTab: Tab = { tabname: 'Security', path: '/security' };
+const legalPageRouteTab: Tab = { tabname: 'Legal', path: '/legal' };
+const privacyPageRouteTab: Tab = { tabname: 'Privacy', path: '/privacy' };
+/**
+ * Maps footer labels to link
+ */
+export const footerLinks: Tab[] = [
+  cookiePreferencesPageRouteTab,
+  securityPageRouteTab,
+  legalPageRouteTab,
+  privacyPageRouteTab,
+];

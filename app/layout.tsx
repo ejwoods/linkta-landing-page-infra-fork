0e9835979992955786b81ba71bd8ffc8b61e347c
ejwoods <ewoods08@gmail.com>
2024-04-05 14:01:10 -0400

    import mantine styles, MantineProvider
    Mantine's guide for setup with Next recommends importing ColorSchemeScript from mantine/core as well. Not importing this now because it's not currently being used.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import '@mantine/core/styles.css';
 import Footer from './components/layout/Footer';
 import LinktaLogoWithText from './components/layout/LinktaLogoWithText';
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 
 const inter = Inter({ subsets: ['latin'] });
 

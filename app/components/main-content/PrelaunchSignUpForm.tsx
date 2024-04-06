1c4548055a6565cf3181757efd77715f91dc44d2
ejwoods <ewoods08@gmail.com>
2024-04-06 01:15:41 -0400

    use useForm hook to set initial form state and validation for email field

--- app/components/main-content/PrelaunchSignUpForm.tsx
+++ app/components/main-content/PrelaunchSignUpForm.tsx
@@ -4,9 +4,21 @@ import { TextInput, Checkbox, Button, Group, Box } from '@mantine/core';
 import { useForm } from '@mantine/form';
 
 export default function PrelaunchSignUpForm() {
+
+  const form = useForm({
+    initialValues: {
+      name: '',
+      email: '',
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+\.\S+$/.test(value) ? null : 'Please enter a valid email address')
+    },
+  });
+
   return (
     <>
-      <div>PrelaunchSignUpForm</div>;
+      <div>PrelaunchSignUpForm</div>
     </>
   )
 }

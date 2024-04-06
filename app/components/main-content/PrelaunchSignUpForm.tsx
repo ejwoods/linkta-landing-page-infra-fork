ed412d69850e46ce7f1f4db28106d9eae40a769e
ejwoods <ewoods08@gmail.com>
2024-04-06 00:22:47 -0400

    import TextInput, Checkbox, Button, Group, and Box components and useForm hook

--- app/components/main-content/PrelaunchSignUpForm.tsx
+++ app/components/main-content/PrelaunchSignUpForm.tsx
@@ -1,5 +1,12 @@
 'use client'
 
+import { TextInput, Checkbox, Button, Group, Box } from '@mantine/core';
+import { useForm } from '@mantine/form';
+
 export default function PrelaunchSignUpForm() {
-  return <div>PrelaunchSignUpForm</div>;
+  return (
+    <>
+      <div>PrelaunchSignUpForm</div>;
+    </>
+  )
 }

83e4bb51b1fefbb437a6e5955217f629e831eb1d
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-07 18:20:53 -0500

    revert back to origin state

--- app/components/main-content/VisualizationTriggerButton.tsx
+++ app/components/main-content/VisualizationTriggerButton.tsx
@@ -1,13 +1,3 @@
-'use client'
-import { useRouter } from 'next/navigation';
-import { Button } from '@mantine/core';
-
 export default function VisualizationTriggerButton() {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push('/early-access');
-  };
-
-  return <Button onClick={handleClick}>Join Our Journey</Button>;;
+  return <div>VisualizationTriggerButton</div>
 }

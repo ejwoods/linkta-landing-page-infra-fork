14a66e820711f6a7253e9b40ec6cd79bbdd435a1
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-07 12:34:11 -0500

    add set time out logic to render submission status for 3s

--- app/early-access/page.tsx
+++ app/early-access/page.tsx
@@ -1,11 +1,32 @@
+'use client'
+import { useState, useEffect, useReducer } from 'react';
 import PrelaunchSignUpForm from '../components/main-content/PrelaunchSignUpForm';
 import SubmissionStatus from '../components/main-content/SubmissionStatusPopup';
 
 export default function PreLaunchSignupFlowContainer() {
+  const [showSubmissionStatus, setShowSubmissionStatus] = useState(false);
+
+  const handleFormSubmit = () => {
+    // more form submittion logic will be added here
+    // pass event handler to PrelaunchSignUpForm
+    setShowSubmissionStatus(true);
+  }
+
+  useEffect(() => {
+    if (showSubmissionStatus) {
+      const timeoutId = setTimeout(() => {
+      // add redirect logic
+      }, 3000);
+
+      // Cleanup timeoutId if component unmounts
+      return () => clearTimeout(timeoutId);
+    }
+  }, [showSubmissionStatus]);
+
   return (
     <main>
-      <PrelaunchSignUpForm />
-      <SubmissionStatus />
+      {!showSubmissionStatus && <PrelaunchSignUpForm onSubmit={handleFormSubmit} />}
+      {showSubmissionStatus && <SubmissionStatus />}
     </main>
   );
 }

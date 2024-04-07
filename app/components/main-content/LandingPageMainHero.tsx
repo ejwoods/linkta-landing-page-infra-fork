b7e1e29e60709e69689445692297301fadc8e01d
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-07 18:21:53 -0500

    remove visualization trigger button from home and implement button in landing page main hero

--- app/components/main-content/LandingPageMainHero.tsx
+++ app/components/main-content/LandingPageMainHero.tsx
@@ -1,3 +1,18 @@
+'use client'
+import { useRouter } from 'next/navigation';
+import UniversalButton from '../common/UniversalButton';
+
 export default function LandingPageMainHero() {
-  return <div>LandingPageMainHero</div>;
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/early-access');
+  };
+
+  return (
+    <main>
+      <div>LandingPageMainHero</div>
+      <UniversalButton onClick={handleClick} label='Join Our Journey'/>
+    </main>
+  )
 }

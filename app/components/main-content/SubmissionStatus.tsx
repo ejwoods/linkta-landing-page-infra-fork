2f7b585c40107ae056b85c107f667f1c0ddb37fe
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-07 14:53:55 -0500

    fix eslint wanrning

--- app/components/main-content/SubmissionStatus.tsx
+++ app/components/main-content/SubmissionStatus.tsx
@@ -5,11 +5,11 @@ export default function SubmissionStatus() {
     <article aria-live="polite">
       <h1>Thank you!</h1>
       <div>animated heart placeholder</div>
-      <p>We're thrilled to have you join us on this exciting journey!</p>
-      <p>(Our demo is almost ready, and we can't wait for you to experience it firsthand.)</p>
+      <p>We&apos;re thrilled to have you join us on this exciting journey!</p>
+      <p>(Our demo is almost ready, and we can&apos;t wait for you to experience it firsthand.)</p>
       <div>green paper plane placeholder</div>
       <p>Keep an eye on your inbox for an <strong>exclusive invitation</strong> coming your way soon!</p>
-      <p>Got questions? We’re here to help! Shoot us an email at <a href={`mailto:${linktaEmail}`}>{linktaEmail}</a></p>
+      <p>Got questions? We&apos;re here to help! Shoot us an email at <a href={`mailto:${linktaEmail}`}>{linktaEmail}</a></p>
     </article>
   )
 }

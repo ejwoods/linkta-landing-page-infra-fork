71eee4a4f51849bafa95023d105c9b0ef239fa7c
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-07 11:30:05 -0500

    add text

--- app/components/main-content/SubmissionStatus.tsx
+++ app/components/main-content/SubmissionStatus.tsx
@@ -1,3 +1,13 @@
 export default function SubmissionStatus() {
-  return <div>SubmissionStatus</div>;
+  return (
+    <div>
+      <h1>Thank you!</h1>
+      <div>animinated heart placeholder</div>
+      <p>We're super excited to have you on board!</p>
+      <p>Our demo is in the final states of preparation, and we can't wait to share it with you. nz</p>
+      <div>green paper plane placeholder</div>
+      <p>Keep an eye for an <strong>exclusive invitation</strong> as soon as it's ready!</p>
+      <p>If you have any questions, don't hesitate to contact us at info@linkta.org</p>
+    </div>
+  )
 }

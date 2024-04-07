5115591dced2218b07d47f6ee1bc2186b1360ecb
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-07 11:42:39 -0500

    fix typos

--- app/components/main-content/SubmissionStatus.tsx
+++ app/components/main-content/SubmissionStatus.tsx
@@ -2,12 +2,12 @@ export default function SubmissionStatus() {
   return (
     <article aria-live="polite">
       <h1>Thank you!</h1>
-      <div>animinated heart placeholder</div>
+      <div>animated heart placeholder</div>
       <p>We're super excited to have you on board!</p>
-      <p>(Our demo is in the final states of preparation, and we can't wait to share it with you. nz)</p>
+      <p>(Our demo is in the final stages of preparation, and we can't wait to share it with you.)</p>
       <div>green paper plane placeholder</div>
-      <p>Keep an eye for an <strong>exclusive invitation</strong> as soon as it's ready!</p>
-      <p>If you have any questions, don't hesitate to contact us at <a href="mailto: info@linkta.org">info@linkta.org</a></p>
+      <p>Keep an eye out for an <strong>exclusive invitation</strong> as soon as it's ready!</p>
+      <p>If you have any questions, don't hesitate to contact us at <a href="mailto:info@linkta.org">info@linkta.org</a></p>
     </article>
   )
 }

9b35e0daec3f4e790b986db3a3da0c1c7bc02e2c
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-07 13:13:16 -0500

    replace with flow state

--- app/early-access/page.tsx
+++ app/early-access/page.tsx
@@ -4,11 +4,13 @@ import { useRouter } from 'next/navigation'
 import PrelaunchSignUpForm from '../components/main-content/PrelaunchSignUpForm';
 import SubmissionStatus from '../components/main-content/SubmissionStatusPopup';
 
+type FlowState = 'form' | 'submissionStatus';
+
 export default function PreLaunchSignupFlowContainer() {
-  const [showSubmissionStatus, setShowSubmissionStatus] = useState(false);
+  const [flowState, setFlowState] = useState<FlowState>('form');
   const router = useRouter()
 
-  const handleFormSubmit = () => {
+  const handleFormSubmit = () : void => {
     // more form submittion logic will be added here
     // pass event handler to PrelaunchSignUpForm
     setShowSubmissionStatus(true);
@@ -16,7 +18,7 @@ export default function PreLaunchSignupFlowContainer() {
 
   useEffect(() => {
     if (showSubmissionStatus) {
-      const timeoutId = setTimeout(() => {
+      const timeoutId: NodeJS.Timeout = setTimeout(() => {
         router.push('/');
       }, 3000);
 
@@ -27,7 +29,11 @@ export default function PreLaunchSignupFlowContainer() {
 
   return (
     <main>
-      {showSubmissionStatus ? <SubmissionStatus /> : <PrelaunchSignUpForm onSubmit={handleFormSubmit} />}
+      {flowState === 'form' ? (
+        <PrelaunchSignUpForm onSubmit={handleFormSubmit} />
+      ) : (
+        <SubmissionStatus />
+      )}
     </main>
   );
 }

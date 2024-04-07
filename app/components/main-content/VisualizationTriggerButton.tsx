a97e5b589ea1ee348ceb5109ba5965c4324af2c7
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-07 18:05:16 -0500

    add routng functionality

--- app/components/main-content/VisualizationTriggerButton.tsx
+++ app/components/main-content/VisualizationTriggerButton.tsx
@@ -1,5 +1,13 @@
+'use client'
+import { useRouter } from 'next/navigation';
 import { Button } from '@mantine/core';
 
 export default function VisualizationTriggerButton() {
-  return <Button>Button</Button>;;
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/early-access');
+  };
+
+  return <Button onClick={handleClick}>Join Our Journey</Button>;;
 }

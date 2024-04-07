0d1fe746a79e744c75cd4bf01f3a6dc5b79b08a3
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-07 12:11:50 -0500

    remove header & footer from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Footer from './components/layout/Footer';
-import LinktaLogoWithText from './components/layout/LinktaLogoWithText';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,15 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <header>
-          <LinktaLogoWithText />
-        </header>
-        <main>{children}</main>
-        <footer>
-          <Footer />
-        </footer>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }

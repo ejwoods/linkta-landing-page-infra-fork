5a49843c03a034c5d1806cc98f7a9639b761db8c
ChTiSh <94409079+ChTiSh@users.noreply.github.com>
2024-04-07 23:53:58 -0700

    Add: Proposed Key Value Proposition Tagline

--- app/components/main-content/LandingPageTreeVisualizationPanel.tsx
+++ app/components/main-content/LandingPageTreeVisualizationPanel.tsx
@@ -3,6 +3,7 @@ export default function LandingPageTreeVisualizationPanel() {
     <div>
       <div>tree placeholder</div>
       <h2>Make Learning Easier</h2>
+      <h4>Organize, structure, and navigate knowledge with the help of AI</h4>
     </div>
   );
 }

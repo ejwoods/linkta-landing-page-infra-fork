76d98918cfb28df493c4c5906d139270d7a6247e
Andrew Lo <andrewlo957@gmail.com>
2024-04-08 18:16:02 -0700

    set parameters for Error function; set up useEffect

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,19 @@
 'use client';
 
-export default function Error() {
-  return <div>Error</div>;
+import { useEffect } from "react";
+/**
+ * Under the hood, error.tsx creates a React Error Boundary that wraps its child segments. The Error component is used as the fallback component, which is rendered if an error is thrown. 
+ * For more context on Error Boundary: https://react.dev/reference/react/Component#catching-rendering-errors-with-an-error-boundary
+ */
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
 }

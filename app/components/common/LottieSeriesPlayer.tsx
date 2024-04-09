bce69ad4c34faf54af11a2d4ecb42ef66d10eb21
ChTiSh <94409079+ChTiSh@users.noreply.github.com>
2024-04-08 21:25:58 -0700

    Change LottieSeriesPlayer.tsx back

--- app/components/common/LottieSeriesPlayer.tsx
+++ app/components/common/LottieSeriesPlayer.tsx
@@ -1,35 +1,33 @@
-{
-  "name": "landing-page",
-  "version": "0.1.0",
-  "private": true,
-  "scripts": {
-    "dev": "next dev",
-    "build": "next build",
-    "start": "next start",
-    "lint": "next lint",
-    "cypress-open": "npx cypress open",
-    "cypress-run": "npx cypress run"
-  },
-  "dependencies": {
-    "@lottiefiles/react-lottie-player": "^3.5.3",
-    "@mantine/core": "^7.7.1",
-    "@mantine/hooks": "^7.7.1",
-    "next": "14.1.3",
-    "react": "^18",
-    "react-dom": "^18"
-  },
-  "devDependencies": {
-    "@types/node": "^20",
-    "@types/react": "^18",
-    "@types/react-dom": "^18",
-    "autoprefixer": "^10.0.1",
-    "cypress": "^13.7.2",
-    "eslint": "^8",
-    "eslint-config-next": "14.1.3",
-    "eslint-config-prettier": "^9.1.0",
-    "postcss": "^8",
-    "prettier": "3.2.5",
-    "tailwindcss": "^3.3.0",
-    "typescript": "^5"
+import { useState, useEffect,  FC } from 'react';
+import { LottieAnimation } from '../../components/main-content/LandingPageTreeVisualizationPanel';
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+
+
+interface LottieSeriesPlayerProps {
+    animationData: LottieAnimation[];
   }
+
+const LottieSeriesPlayer: FC<LottieSeriesPlayerProps> = ({animationData}) => {
+    const [currentAnimation, setCurrentAnimation] = useState(0);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCurrentAnimation(prevIndex => prevIndex >= animationData.length - 1 ? 0 : prevIndex + 1);
+        }, animationData[currentAnimation].duration); 
+
+        return () => clearTimeout(timer);
+    }, [currentAnimation, animationData]);
+
+    return (
+        <Player
+        autoplay
+        loop={false}
+        src={animationData[currentAnimation].src}
+        style={{ width: '320px' }} 
+      >
+        <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
+      </Player>
+    );
 }
+
+export default LottieSeriesPlayer;

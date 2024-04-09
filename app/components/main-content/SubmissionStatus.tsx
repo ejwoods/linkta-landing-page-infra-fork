ab7bc520648379666c8340c90943dc471c8ced6f
ChTiSh <chentina.shen@gmail.com>
2024-04-08 21:01:32 -0700

    update the player for the submission status

--- app/components/main-content/SubmissionStatus.tsx
+++ app/components/main-content/SubmissionStatus.tsx
@@ -1,46 +1,34 @@
 import {useRef, useEffect} from 'react';
 import heartAnimationData from "../../../public/lottiefiles/heart.json";
 import mailAnimationData from "../../../public/lottiefiles/mail-plane.json";
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
 export default function SubmissionStatus() {
   const linktaEmail = 'info@linkta.org';
-  const heartRef = useRef<any>(null);
-  const mailRef = useRef<any>(null);
-
-  useEffect(() => {
-    const loadLottieFiles = async () => {
-      await import("@lottiefiles/lottie-player");
-      if (heartRef.current) {
-        heartRef.current.load(JSON.stringify(heartAnimationData));
-      }
-      if (mailRef.current) {
-        mailRef.current.load(JSON.stringify(mailAnimationData));
-      }
-    };
-    loadLottieFiles();
-  }, []);
+  
   return (
     <article aria-live="polite">
       <h1>Thank you!</h1>
-      <div><lottie-player
-          id="flyingHeart"
-          ref={heartRef}
+      <div><Player
           autoplay
-          loop
-          mode="normal"
+          loop={false}
           src={heartAnimationData}
           style={{ width: "50px", height: "50px" }}
-        ></lottie-player></div>
+        >
+          <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
+          </Player></div>
       <p>We&apos;re thrilled to have you join us on this exciting journey!</p>
       <p>(Our demo is almost ready, and we can&apos;t wait for you to experience it firsthand.)</p>
-      <div><lottie-player
-          id="flyingMail"
-          ref={mailRef}
+      <div>
+        <Player
           autoplay
-          loop
-          mode="normal"
+          loop={true}
           src={mailAnimationData}
           style={{ width: "300px", height: "300px" }}
-        ></lottie-player></div>
+        >
+          <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
+        </Player>
+        
+        </div>
       <p>Keep an eye on your inbox for an <strong>exclusive invitation</strong> coming your way soon!</p>
       <p>Got questions? We&apos;re here to help! Shoot us an email at <a href={`mailto:${linktaEmail}`}>{linktaEmail}</a></p>
     </article>

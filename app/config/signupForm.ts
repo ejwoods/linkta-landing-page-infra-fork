4fc9a206d8b05b3c766cd0b96ea4e1ef1122a4a6
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-17 09:18:58 -0500

    update labels to improve readabilit

--- app/config/signupForm.ts
+++ app/config/signupForm.ts
@@ -21,11 +21,11 @@ function createConfigItem(
 }
 
 const textInputConfig: TextInputConfig[] = [
-  createConfigItem('name', 'Name', 'Enter your name', validateMinLength(1, 'Name'), true),
-  createConfigItem('email', 'Email', 'Enter your email', validateEmail, true),
-  createConfigItem('interests', 'Interests', 'Enter your interests', validateMinLength(3, 'Interests')),
-  createConfigItem('source', 'How did you hear about us?', 'Enter your source', validateMinLength(3, 'Source')),
-  createConfigItem('features', 'What features are you most interested in?', 'Describe features', validateMinLength(5, 'Features'))
+  createConfigItem('name', 'Name (required)', 'Enter your name', validateMinLength(1, 'Name'), true),
+  createConfigItem('email', 'Email (required)', 'Enter your email', validateEmail, true),
+  createConfigItem('interests', 'Interests (optional)', 'Enter your interests', validateMinLength(3, 'Interests')),
+  createConfigItem('source', 'How did you hear about us? (optional)', 'Enter your source', validateMinLength(3, 'Source')),
+  createConfigItem('features', 'What features are you most interested in? (optional)', 'Describe features', validateMinLength(5, 'Features'))
 ];
 
 export default textInputConfig;

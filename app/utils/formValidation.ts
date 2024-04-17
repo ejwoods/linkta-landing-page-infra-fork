7087542af1a087575b613bae13aaf912ae515498
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-17 09:50:08 -0500

    refactor validation helpers for more robust validation checking

--- app/utils/formValidation.ts
+++ app/utils/formValidation.ts
@@ -1,7 +1,17 @@
 export const validateMinLength = (minLength: number, field: string) => (value: string) => {
-  return value.length < minLength ? `${field} must have at least ${minLength} letters. Please try again.` : null;
+  return value.trim().length < minLength ? `${field} must have at least ${minLength} letters. Please try again.` : null;
 }
 
 export const validateEmail = (value: string) => {
-  return /^\S+@\S+\.\S+$/.test(value) ? null : 'Invalid email format. Please enter a valid email address.';
+  if (value.trim().length < 3) {
+    return 'Email address must have at least 3 letters. Pleaes try again.';
+  }
+
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (!emailRegex.test(value)) {
+    return 'Invalid email format. Please check for missing "@" or "." symbols, or invalid characters and try again';
+  }
+
+  return null;
 }

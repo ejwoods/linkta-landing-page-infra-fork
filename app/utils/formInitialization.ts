8131e543525a3fa22f6e2a501af61d9285d5edb5
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-18 06:41:48 -0500

    fix format

--- app/utils/formInitialization.ts
+++ app/utils/formInitialization.ts
@@ -6,33 +6,35 @@ interface ConfigItem {
 }
 
 /**
-* Generates initial form values based on the provided config.
-* @param config - Array of ConfigItem objects.
-* @returns Object with field names as keys and empty strings as values.
-*/
+ * Generates initial form values based on the provided config.
+ * @param config - Array of ConfigItem objects.
+ * @returns Object with field names as keys and empty strings as values.
+ */
 export const generateInitialValues = (config: ConfigItem[]): FormValues => {
   const initialValues = {} as FormValues;
 
-  config.forEach(item => {
+  config.forEach((item) => {
     initialValues[item.field] = '';
   });
 
   return initialValues;
-}
+};
 
 /**
-* Generates validation rules based on the provided config.
-* @param config - Array of ConfigItem objects.
-* @returns Object with field names as keys and validation functions as values.
-*/
-export const generateValidationRules = (config: ConfigItem[]): Record<string, ValidationFunction> => {
+ * Generates validation rules based on the provided config.
+ * @param config - Array of ConfigItem objects.
+ * @returns Object with field names as keys and validation functions as values.
+ */
+export const generateValidationRules = (
+  config: ConfigItem[]
+): Record<string, ValidationFunction> => {
   const validationRules: Record<string, ValidationFunction> = {};
 
-  config.forEach(item => {
-    if(item.validate){
+  config.forEach((item) => {
+    if (item.validate) {
       validationRules[item.field] = item.validate;
     }
   });
 
   return validationRules;
-}
+};
--- app/utils/formValidation.ts
+++ app/utils/formValidation.ts
@@ -4,13 +4,14 @@
  * @param {string} field - The name of the field to validate.
  * @returns {string|null} An error message if validation fails, otherwise null.
  */
-export const validateMinLength = (minLength: number, field: string) => (value: string) => {
-  if (value.trim().length < minLength) {
-    const pluralSuffix = minLength > 1 ? 's' : '';
-    return `${field} must have at least ${minLength} letter${pluralSuffix}. Please try again.`;
-  }
-  return null;
-}
+export const validateMinLength =
+  (minLength: number, field: string) => (value: string) => {
+    if (value.trim().length < minLength) {
+      const pluralSuffix = minLength > 1 ? 's' : '';
+      return `${field} must have at least ${minLength} letter${pluralSuffix}. Please try again.`;
+    }
+    return null;
+  };
 
 /**
  * Validates the format of an email address.
@@ -18,9 +19,12 @@ export const validateMinLength = (minLength: number, field: string) => (value: s
  * @returns {string|null} An error message if the email format is invalid, otherwise null.
  */
 export const validateEmailFormat = (value: string) => {
-  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const emailRegex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  return !emailRegex.test(value) ? 'Invalid email format. Please check for missing "@" or "." symbols, or invalid characters and try again' : null;
+  return !emailRegex.test(value)
+    ? 'Invalid email format. Please check for missing "@" or "." symbols, or invalid characters and try again'
+    : null;
 };
 
 /**

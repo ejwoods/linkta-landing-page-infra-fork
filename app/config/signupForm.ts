7655269e5f6a65e18c5eb7a5307b5c442bc54479
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-18 06:38:41 -0500

    change fun dec to fun exp

--- app/config/signupForm.ts
+++ app/config/signupForm.ts
@@ -1,4 +1,4 @@
-import { validateEmail, validateMinLength } from "../utils/formValidation";
+import { validateEmail, validateMinLength } from '../utils/formValidation';
 import type { FormValues, ValidationFunction } from '../types/signupForm';
 
 interface TextInputConfig {
@@ -18,15 +18,21 @@ interface TextInputConfig {
  * @param {boolean} [required=false] - Indicates if the field is required.
  * @returns {TextInputConfig} The configuration object for a text input field.
  */
-function createConfigItem(
+const createConfigItem: (
   field: keyof FormValues,
   label: string,
   placeholder: string,
   validate: ValidationFunction,
+  required?: boolean
+) => TextInputConfig = (
+  field,
+  label,
+  placeholder,
+  validate,
   required = false
-): TextInputConfig {
+) => {
   return { field, label, placeholder, validate, required };
-}
+};
 
 /**
  * Configuration array for text input fields used in the PreLaunchSignUpForm.
@@ -34,11 +40,38 @@ function createConfigItem(
  * @type {TextInputConfig[]}
  */
 const textInputConfig: TextInputConfig[] = [
-  createConfigItem('name', 'Name (required)', 'Enter your name', validateMinLength(1, 'Name'), true),
-  createConfigItem('email', 'Email (required)', 'Enter your email', validateEmail, true),
-  createConfigItem('interests', 'Interests (optional)', 'Enter your interests', validateMinLength(3, 'Interests')),
-  createConfigItem('source', 'How did you hear about us? (optional)', 'Enter your source', validateMinLength(3, 'Source')),
-  createConfigItem('features', 'What features are you most interested in? (optional)', 'Describe features', validateMinLength(5, 'Features'))
+  createConfigItem(
+    'name',
+    'Name (required)',
+    'Enter your name',
+    validateMinLength(1, 'Name'),
+    true
+  ),
+  createConfigItem(
+    'email',
+    'Email (required)',
+    'Enter your email',
+    validateEmail,
+    true
+  ),
+  createConfigItem(
+    'interests',
+    'Interests (optional)',
+    'Enter your interests',
+    validateMinLength(3, 'Interests')
+  ),
+  createConfigItem(
+    'source',
+    'How did you hear about us? (optional)',
+    'Enter your source',
+    validateMinLength(3, 'Source')
+  ),
+  createConfigItem(
+    'features',
+    'What features are you most interested in? (optional)',
+    'Describe features',
+    validateMinLength(5, 'Features')
+  ),
 ];
 
 export default textInputConfig;

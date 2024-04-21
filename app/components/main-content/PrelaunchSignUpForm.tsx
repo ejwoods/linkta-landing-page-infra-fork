5ab8701bb5ac6f0d7e660e858e1ea3f9172a1d76
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-21 09:29:02 -0500

    update error msg

--- app/components/main-content/PrelaunchSignUpForm.tsx
+++ app/components/main-content/PrelaunchSignUpForm.tsx
@@ -57,7 +57,7 @@ const PrelaunchSignUpForm: React.FC<PrelaunchSignUpFormProps> = ({ setFlowState 
     try {
       await storeUserDataIfNew(sanitizedUserData.email, sanitizedUserData);
     } catch (error) {
-      console.error('Failed to store user data:');
+      console.error('Error cheking user data existence of store user data.');
     }
 
     setFlowState('confirmed');

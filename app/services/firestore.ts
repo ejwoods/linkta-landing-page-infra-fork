83f1f148067cdeca7744ccd0d5b76e1b9da0c251
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-20 19:30:36 -0500

    add jsdocs

--- app/services/firestore.ts
+++ app/services/firestore.ts
@@ -6,6 +6,7 @@ import {
 } from 'firebase/firestore';
 import type { userDataSanitization } from '../schemas/userDataSanitizationSchema';
 import { db } from '../config/firebase';
+
 /**
  * Asynchronously checks the existence of a Firestore document.
  *
@@ -24,6 +25,13 @@ export const doesUserDataExist = async (
   }
 };
 
+/**
+ * Stores user data in Firestore if the user does not already exist.
+ *
+ * @param {string} userId - The unique identifier for the user.
+ * @param {userDataSanitization} userData - The sanitized user data to be stored.
+ * @returns {Promise<void>} - A promise that resolves when the operation is complete.
+ */
 export const storeUserDataIfNew = async (
   userId: string,
   userData: userDataSanitization
--- app/utils/formInputSanitization.ts
+++ app/utils/formInputSanitization.ts
@@ -1,13 +1,28 @@
 export const ALLOWED_COMMON_CHARS = '\\p{Letter}0-9 ,.\\-&\\[\\]()';
 
+/**
+ * Removes empty strings from an array.
+ * @param {string[]} array - Array to filter.
+ * @returns {string[]} Filtered array with non-empty strings.
+ */
 export const removeEmptyItems = (array: string[]): string[] => {
   return array.length ? array.filter((item) => item.length > 0) : [];
 };
 
-export const removeExtraWhiteSpaces = (input: string) => {
+/**
+ * Trims and replaces multiple whitespace characters with a single space.
+ * @param {string} input - The string to process.
+ * @returns {string} The processed string.
+ */
+export const removeExtraWhiteSpaces = (input: string):string => {
   return input.trim().replace(/\s+/g, ' ');
 };
 
+/**
+ * Removes characters that are not allowed from a string.
+ * @param {string} text - The string to sanitize.
+ * @returns {string} The sanitized string.
+ */
 export const removeSpecialCharacters = (
   text: string
 ): string => {
@@ -15,6 +30,11 @@ export const removeSpecialCharacters = (
   return text.replace(allowedCharsRegex, '').trim();
 };
 
+/**
+ * Splits a string by commas, cleans it of unwanted characters, and removes empty items.
+ * @param {string | null | undefined} input - The input string to process.
+ * @returns {string[]} Array of processed strings.
+ */
 export const parseAndCleanInput = (
   input: string | null | undefined
 ): string[] => {

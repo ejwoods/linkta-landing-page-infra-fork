4105baaa7b30c302c5239d17494df8aa074b529a
ejwoods <ewoods08@gmail.com>
2024-04-24 20:23:41 -0400

    add TextInput component test suite

--- __tests__/unit/components/PrelaunchSignUpForm/TextInput.test.tsx
+++ __tests__/unit/components/PrelaunchSignUpForm/TextInput.test.tsx
@@ -1,3 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import PrelaunchSignUpForm from '../../../../app/components/main-content/PrelaunchSignUpForm'
 
+describe('TextInput components in PrelaunchSignUpForm', () => {
+
+});

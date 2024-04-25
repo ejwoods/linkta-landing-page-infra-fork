adc1bdc84d8868b07055c0bf240951b2572d7d6f
ejwoods <ewoods08@gmail.com>
2024-04-25 02:20:22 -0400

    Configure Jest and TypeScript to simplify test-utils integration
    - Added index.ts to the test-utils directory to export utilities globally
    - Updated jest.config.ts with path alias for 'test-utils'
    - Modified tsconfig.json to resolve 'test-utils' imports

---
+++ test-utils/index.ts
@@ -0,0 +1,3 @@
+// Allows for convenient access to standard React Testing Library utilities when importing customRender
+export * from '@testing-library/react';
+export { render } from './customRender';

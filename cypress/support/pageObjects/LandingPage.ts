ce97dd5b90737b3dd23fd41a8d524d34ad58a634
Martin Carmona <martin.e.carmona@gmail.com>
2024-04-25 16:38:23 -0700

    add wait for page to load logic

--- cypress/support/pageObjects/LandingPage.ts
+++ cypress/support/pageObjects/LandingPage.ts
@@ -1,10 +1,11 @@
 export default class LandingPage {
-  private readonly url = 'http://localhost:3000';
+  private readonly landingPageUrl = 'http://localhost:3000';
+  private readonly earlyAccessUrl = `${this.landingPageUrl}/early-access`;
   private readonly headerTitleId = '#linkta-title';
   private readonly earlyAccessPageButtonId = '#early-access-button';
 
   public open(): void {
-    cy.visit(this.url);
+    cy.visit(this.landingPageUrl);
   }
 
   public getHeaderTitle(): Cypress.Chainable {
@@ -15,7 +16,18 @@ export default class LandingPage {
     return cy.get(this.earlyAccessPageButtonId);
   }
 
+  private retryIfEarlyAccessNotLoaded(attempts: number = 3): void {
+    cy.url().then((currentUrl) => {
+      if (currentUrl !== this.earlyAccessUrl && attempts > 0) {
+        cy.get(this.earlyAccessPageButtonId).click();
+        cy.wait(500);
+        this.retryIfEarlyAccessNotLoaded(attempts - 1);
+      }
+    });
+  }
+
   public navigateToEarlyAccessPage(): void {
     this.getEarlyAccessButton().click();
+    this.retryIfEarlyAccessNotLoaded();
   }
 }

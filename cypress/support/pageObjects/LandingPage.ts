dd98dd62414cc5469e190f48f97c82d90c0af4a5
Martin Carmona <martin.e.carmona@gmail.com>
2024-04-25 13:52:10 -0700

    add method to navigate to early access page

--- cypress/support/pageObjects/LandingPage.ts
+++ cypress/support/pageObjects/LandingPage.ts
@@ -1,6 +1,7 @@
 export default class LandingPage {
   private readonly url = 'http://localhost:3000';
   private readonly headerTitleId = '#linkta-title';
+  private readonly earlyAccessPageButtonId = '#early-access-button';
 
   public open(): void {
     cy.visit(this.url);
@@ -8,5 +9,13 @@ export default class LandingPage {
 
   public getHeaderTitle(): Cypress.Chainable {
     return cy.get(this.headerTitleId);
+  }
+
+  private getEarlyAccessButton(): Cypress.Chainable {
+    return cy.get(this.earlyAccessPageButtonId);
+  }
+
+  public navigateToEarlyAccessPage(): void {
+    this.getEarlyAccessButton().click();
   }
 }

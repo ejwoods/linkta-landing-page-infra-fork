0f36775148856c2954904d2c9edd08adc3511101
ejwoods <ewoods08@gmail.com>
2024-04-24 21:08:55 -0400

    add beforeEach invocation to render PrelaunchSignUpForm component before each test
    This ensures that each test runs with freshly rendered components

--- __tests__/unit/components/PrelaunchSignUpForm/TextInput.test.tsx
+++ __tests__/unit/components/PrelaunchSignUpForm/TextInput.test.tsx
@@ -1,6 +1,14 @@
 import { render, screen } from '@testing-library/react'
-import PrelaunchSignUpForm from '../../../../app/components/main-content/PrelaunchSignUpForm'
+import PrelaunchSignUpForm, { PrelaunchSignUpFormProps } from '../../../../app/components/main-content/PrelaunchSignUpForm'
 
 describe('TextInput components in PrelaunchSignUpForm', () => {
+  beforeEach(() => {
+    const mockSetFlowState: jest.Mock = jest.fn();
+    const props: PrelaunchSignUpFormProps = {
+      setFlowState: mockSetFlowState,
+    };
+
+    render(<PrelaunchSignUpForm {...props} />)
+  });
 
 });

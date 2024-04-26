75b793a1411fcef2c28860bbbbeb952a97f1b6eb
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-26 16:47:42 -0500

    incorporate lazy load and susppense for spline scene

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 'use client';
-import { useState } from 'react';
+import { Suspense, lazy, useState } from 'react';
 import LinktaLogoWithText from './components/layout/LinktaLogoWithText';
 import SubmissionStatus from './components/main-content/SubmissionStatus';
 import PrelaunchSignUpForm from './components/main-content/PrelaunchSignUpForm';
-import LandingPageTreeVisualizationPanel from './components/main-content/LandingPageTreeVisualizationPanel';
+import Loading from './loading';
+
+const LandingPageTreeVisualizationPanel = lazy(() => import('./components/main-content/LandingPageTreeVisualizationPanel'));
 
 export default function Home() {
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -21,7 +23,9 @@ export default function Home() {
           {isSubmitted ? <SubmissionStatus /> : <PrelaunchSignUpForm handleSuccessfulSubmit={handleSuccessfulSubmit} />}
         </div>
           <div className="flex flex-col justify-center sm:flex-row-reverse">
-          <LandingPageTreeVisualizationPanel />
+            <Suspense fallback={<Loading />}>
+              <LandingPageTreeVisualizationPanel />
+            </Suspense>
           </div>
       </main>
     </>

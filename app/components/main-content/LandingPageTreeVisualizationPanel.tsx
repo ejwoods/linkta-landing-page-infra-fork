65214e5dd7781871d557501ce1e05bb168453cec
ChTiSh <chentina.shen@gmail.com>
2024-04-25 21:08:13 -0700

    fix the tree size glitch bug and add the aria label as suggested

--- app/components/main-content/LandingPageTreeVisualizationPanel.tsx
+++ app/components/main-content/LandingPageTreeVisualizationPanel.tsx
@@ -6,7 +6,8 @@ export default function LandingPageTreeVisualizationPanel() {
   return (
     <div>
       <div className="absolute">
-      <Spline className="relative scale-[1.75]" scene="https://prod.spline.design/o3aArfp9O67BHMQ2/scene.splinecode" />
+        <Spline aria-label="Interactive 3D model of a tree, which you can rotate and click to explore different functionalities."
+        className="relative scale-[1.25]" scene="https://prod.spline.design/igFoxNahg10ecXKH/scene.splinecode" />
       </div>
       <h2 className="font-serif text-lg font-bold py-2">Make Learning Easier</h2>
       <h4 className="text-center">Organize, structure, and navigate knowledge with the help of AI</h4>

b66cebc1e4a004d00c0442dc330666060c198a12
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-26 16:07:55 -0500

    insert prelaunchsignupform and submissionstatus componnent

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
 import { useState } from 'react';
 import LinktaLogoWithText from './components/layout/LinktaLogoWithText';
 import LandingPageMainHero from './components/main-content/LandingPageMainHero';
+import SubmissionStatus from './components/main-content/SubmissionStatus';
+import PrelaunchSignUpForm from './components/main-content/PrelaunchSignUpForm';
 
 export default function Home() {
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const handleSuccessfulSubmit = () => {
-      setIsSubmitted(true);
-  };
+  const handleSuccessfulSubmit = () => setIsSubmitted(true);
 
   return (
-    <div>
-        <div className="sm:hidden ml-0">
-          <LinktaLogoWithText />
+    <>
+      <div className="sm:hidden ml-0">
+        <LinktaLogoWithText />
+      </div>
+      <div>
+        <div>
+          <>ValueProposition Placeholder</>
+          {isSubmitted ? <SubmissionStatus /> : <PrelaunchSignUpForm handleSuccessfulSubmit={handleSuccessfulSubmit} />}
         </div>
-        <LandingPageMainHero />
-    </div>
+            <LandingPageMainHero />
+      </div>
+    </>
   );
 }

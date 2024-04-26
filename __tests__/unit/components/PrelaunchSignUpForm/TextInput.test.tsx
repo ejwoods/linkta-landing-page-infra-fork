b73d0657de0f9cae98e297d844b879849ffa5b04
ejwoods <ewoods08@gmail.com>
2024-04-26 01:06:15 -0400

    refactor: replace forEach with a setup function that's called in each test
    Replaced beforeEach invocation with a setup function, to be called in each test, in order to align with Testing Library best practices which "discourage rendering or using any userEvent functions outside of the test itself" (https://testing-library.com/docs/user-event/intro/#writing-tests-with-userevent). This change explicitly shows setup for each test, making tests easier for maintainers to understand without needing to reference a beforeEach call.

--- __tests__/unit/components/PrelaunchSignUpForm/TextInput.test.tsx
+++ __tests__/unit/components/PrelaunchSignUpForm/TextInput.test.tsx
@@ -6,28 +6,33 @@ jest.mock('@/app/services/firestore', () => ({
 }));
 
 describe('TextInput components in PrelaunchSignUpForm', () => {
-  beforeEach(() => {
+  const setup = () => {
     const mockSetFlowState: jest.Mock = jest.fn();
     const props: PrelaunchSignUpFormProps = {
       setFlowState: mockSetFlowState,
     };
 
-    render(<PrelaunchSignUpForm {...props} />)
-  });
+    const renderResult = render(<PrelaunchSignUpForm {...props} />)
+    return { renderResult }
+  };
 
   it('renders the name TextInput accessibly and with the correct label', () => {
+    setup();
     expect(screen.getByRole('textbox', { name: 'What is your name? (required)' })).toBeInTheDocument();
   });
   
   it('renders the email TextInput accessibly and with the correct label', () => {
+    setup();
     expect(screen.getByRole('textbox', { name: 'Where can we email you? (required)' })).toBeInTheDocument();
   });  
 
   it('renders the interests TextInput accessibly and with the correct label', () => {
+    setup();
     expect(screen.getByRole('textbox', { name: 'What would you love to learn? (optional)' })).toBeInTheDocument();
   });
 
   it('renders the source TextInput accessibly and with the correct label', () => {
+    setup();
     expect(screen.getByRole('textbox', { name: 'How did you find us? (optional)' })).toBeInTheDocument();
   });
 });

95eadc67d74cdf84ada4d07e8dc4a46f5d0693e4
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-26 15:53:44 -0500

    create submission state

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import LinktaLogoWithText from './components/layout/LinktaLogoWithText';
 import LandingPageMainHero from './components/main-content/LandingPageMainHero';
 
 export default function Home() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSuccessfulSubmit = () => {
+      setIsSubmitted(true);
+  };
+
   return (
     <div>
         <div className="sm:hidden ml-0">

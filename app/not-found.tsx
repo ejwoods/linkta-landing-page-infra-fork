63f0e2aeeffa482db5d93ae41e98a10db1905c7a
Andrew Lo <andrewlo957@gmail.com>
2024-04-26 18:44:56 -0700

    adjusted spacing, changed hyperlink

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className="">
-      <h1 className="mb-8">Oops!</h1>
-      <p className="mb-32">Something went wrong!</p>
-      <p className="">Please <a href="#" className="text-light-text underline hover" onClick={() => window.location.reload()}>click here</a> to refresh the page or contact us at info@linkta.org</p>
+      <h2 className="mb-4 text-3xl font-semibold">Oops!</h2>
+      <p className="mb-64">Something went wrong!</p>
+      <p className="mb-32">Please <a href="/" className="text-light-text underline hover">click here</a> to return home or contact us at info@linkta.org</p>
     </div>
   )
-}
+};

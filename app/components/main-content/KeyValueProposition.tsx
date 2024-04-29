4fd83d3ae268782504e41edd84f83b042557ad49
ChTiSh <chentina.shen@gmail.com>
2024-04-29 16:47:18 -0700

    basic styling done with phone and desktop screens

--- app/components/main-content/KeyValueProposition.tsx
+++ app/components/main-content/KeyValueProposition.tsx
@@ -1,9 +1,9 @@
 export default function KeyValueProposition() {
     return (
-      <section className="pl-4 font-serif font-bold  px-2 mt-12 text-light-black">
-        <h1 className="text-3xl my-2  sm:text-4xl lg:text-5xl xl:text-6xl">Streamline Your Learning with Linkta </h1>
+      <section className="pl-4 font-serif font-bold  px-2 mt-16 text-light-black">
+        <h1 className="text-4xl my-4 sm:text-4xl lg:text-5xl xl:text-6xl">Streamline Your Learning with Linkta </h1>
         <h2 className="hidden sm:block font-sans sm:font-serif  sm:text-light-black sm:mb-2 sm:text-4xl lg:text-5xl xl:text-6xl">Discover the IA Structure of Knowledge with AI</h2>
-        <h4 className="font-sans hidden lg:block sm:text-transparent font-xs text-base sm:font-medium leading-tight text-light-text pb-4 sm:pb-0">Linkta makes learning easier by uncovering the hidden relationships between concepts. Our tool allows you to see the big picture and reveals the connections between ideas, enabling you to focus on what matters most.</h4>
+        <h4 className="font-sans block text-light-text sm:hidden lg:block sm:text-transparent font-sm my-6 font-base sm:font-medium leading-tight text-light-text pb-4 sm:pb-0">Linkta makes learning easier by uncovering the hidden relationships between concepts. Our tool allows you to see the big picture and reveals the connections between ideas, enabling you to focus on what matters most.</h4>
       </section>
     )
 }
--- app/components/main-content/LandingPageTreeVisualizationPanel.tsx
+++ app/components/main-content/LandingPageTreeVisualizationPanel.tsx
@@ -14,15 +14,15 @@ export default function LandingPageTreeVisualizationPanel() {
   }, []);
 
   return (
-    <div className='min-w-[360px] -mt-24 sm:-mt-8 '>
+    <div className='min-w-[360px] -mt-12 sm:-mt-8 '>
       <div className="hidden sm:block mt-4">
         <Spline aria-label="Interactive 3D model of a tree, which you can rotate and click to explore different functionalities."
-        className="w-full -ml-24" scene="https://prod.spline.design/3EDxf9YSTlnBoD1H/scene.splinecode" />
+        className="w-full " scene="https://prod.spline.design/3EDxf9YSTlnBoD1H/scene.splinecode" />
       </div>
       <div className='sm:hidden'>
         <Spline className='' scene="https://prod.spline.design/n30RNsX1c9-QGXnZ/scene.splinecode" />
       </div>
-      <h2 className="block sm:hidden font-serif font-bold text-2xl -mt-36 mb-24 pl-2 text-light-text">Discover the IA Structure of Knowledge with AI</h2>
+      <h2 className="block sm:hidden font-serif font-bold text-3xl -mt-24 mb-24 pl-2 text-light-text">Discover the IA Structure of Knowledge with AI</h2>
     </div>
   );
 }

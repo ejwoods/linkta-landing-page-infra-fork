7bc9b94de1b7bcf8ef33c177bff725049355913a
zhiyi15 <zhiyihuang1994@gmail.com>
2024-04-29 15:29:32 -0700

    relocated ThankYou Component to main-content folder
    Co-authored-by: Jordan Woods <ejwoods@users.noreply.github.com>

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -5,11 +5,10 @@ import { isSignInWithEmailLink } from 'firebase/auth';
 import { auth } from '../config/firebase';
 import { authenticateAndSaveUserDataFromEmailRedirect } from '../services/emailAuth';
 import { useRouter } from 'next/navigation';
-import heartAnimationData from '../../public/lottiefiles/heart.json';
-import mailAnimationData from '../../public/lottiefiles/mail-plane.json';
-import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import loading from '../loading';
+import ThankYou from '../components/main-content/ThankYou';
+
 export default function ThankYou() {
-  const linktaEmail = 'info@linkta.org';
   const [isLoading, setIsLoading] = useState(true);
 
   const router = useRouter();
@@ -37,69 +36,6 @@ export default function ThankYou() {
   return (
     // if isLoading render loading component
     // if !isLoading render:
-    <article aria-live="polite">
-      <h1 className="flex h-[60px] pt-4 align-bottom">
-        <span className="align-text-bottom">Thank you!</span>
-        <Player
-          autoplay
-          loop={false}
-          src={heartAnimationData}
-          style={{
-            width: '50px',
-            height: '50px',
-            background: 'transparent',
-            fillOpacity: '0',
-          }}
-        >
-          <Controls
-            visible={false}
-            buttons={['play', 'repeat', 'frame', 'debug']}
-          />
-        </Player>
-      </h1>
-      <section>
-        <p className="pt-4 text-lg">
-          We&apos;re thrilled to have you join us on this exciting journey!
-        </p>
-        <p className="pt-2 text-xs">
-          (Our demo is almost ready, and we can&apos;t wait for you to
-          experience it firsthand.)
-        </p>
-      </section>
-      <div>
-        <Player
-          autoplay
-          loop={true}
-          src={mailAnimationData}
-          style={{ width: '300px', height: '300px' }}
-        >
-          <Controls
-            visible={false}
-            buttons={['play', 'repeat', 'frame', 'debug']}
-          />
-        </Player>
-      </div>
-      <section>
-        <p>
-          If you&apos;d like to keep up with the project,{' '}
-          <a
-            href="https://www.linkedin.com/company/100947448/"
-            target="_blank"
-          >
-            follow us on LinkedIn!
-          </a>
-        </p>
-      </section>
-      <section>
-        <p className="text-lg">
-          Keep an eye on your inbox for an <strong>exclusive invitation</strong>{' '}
-          coming your way soon!
-        </p>
-        <p className="absolute bottom-10 text-xs">
-          Got questions? We&apos;re here to help! Shoot us an email at{' '}
-          <a href={`mailto:${linktaEmail}`}>{linktaEmail}</a>
-        </p>
-      </section>
-    </article>
+
   );
 }

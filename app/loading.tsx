4894ac78ba988d4a580d91c5f674b8c4314d75f3
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-29 17:00:12 -0500

    remove uncessary bracket

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,5 @@
 import LottieAnimateLoader from "./components/common/LottieAnimateLoader";
 
 export default function Loading() {
-  return (
-    <LottieAnimateLoader />
-  );
+  return <LottieAnimateLoader />;
 }

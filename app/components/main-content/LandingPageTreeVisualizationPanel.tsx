51d8b7843f653794f2cec1d060ad5c2a7d351426
ChTiSh <chentina.shen@gmail.com>
2024-04-29 15:42:17 -0700

    fix the canvas resize bug from spline

--- app/components/main-content/LandingPageTreeVisualizationPanel.tsx
+++ app/components/main-content/LandingPageTreeVisualizationPanel.tsx
@@ -1,7 +1,17 @@
 'use client'
 import Spline from '@splinetool/react-spline';
+import { useEffect } from 'react';
 
 export default function LandingPageTreeVisualizationPanel() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const canvas = document.querySelector('canvas');
+      if (canvas) {
+        canvas.style.width = '430px';
+        canvas.style.height = '626px';
+      }
+    }
+  }, []);
 
   return (
     <div className='min-w-[360px] -mt-24 sm:-mt-8 '>
@@ -10,7 +20,7 @@ export default function LandingPageTreeVisualizationPanel() {
         className="w-full" scene="https://prod.spline.design/3EDxf9YSTlnBoD1H/scene.splinecode" />
       </div>
       <div className='sm:hidden'>
-        <Spline scene="https://prod.spline.design/n30RNsX1c9-QGXnZ/scene.splinecode" />
+        <Spline className='' scene="https://prod.spline.design/n30RNsX1c9-QGXnZ/scene.splinecode" />
       </div>
     </div>
   );

5cc4e0f255654b9a85276f49da23a0fbaa984419
ChTiSh <chentina.shen@gmail.com>
2024-04-29 21:41:31 -0700

    add animation for AI

--- app/components/main-content/KeyValueProposition.tsx
+++ app/components/main-content/KeyValueProposition.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 
 export default function KeyValueProposition() {
-    const [hover, setHover] = useState(false);
+    const [hoverIA, setHoverIA] = useState(false);
+    const [hoverAI, setHoverAI] = useState(false);
 
     return (
         <section className="pl-4 font-serif font-bold px-2 mt-16 mb-10 text-black">
@@ -12,20 +13,35 @@ export default function KeyValueProposition() {
                 Discover the 
                 <span 
                     className="cursor-pointer font-cursive text-8xl -mt-4 inline-block align-text-bottom mx-2 text-light-accent relative"
-                    onMouseEnter={() => setHover(true)} 
-                    onMouseLeave={() => setHover(false)}
+                    onMouseEnter={() => setHoverIA(true)} 
+                    onMouseLeave={() => setHoverIA(false)}
                 >
                     IA
                     <span 
                         className={`absolute -left-24 top-full transition-transform duration-300 ease-in-out origin-top ${
-                            hover ? 'scale-y-100' : 'scale-y-0'
+                            hoverIA ? 'scale-y-100' : 'scale-y-0'
                         }`}
                         style={{ transformOrigin: '0% 0%' }}
                     >
                         <span className="block whitespace-nowrap text-3xl">Information Architecture</span>
                     </span>
                 </span>
-                of Knowledge with the help of <span className='font-cursive text-8xl inline-block align-text-bottom ml-2 text-light-border'>AI</span>
+                of Knowledge with the help of 
+                <span 
+                    className="cursor-pointer font-cursive text-8xl -mt-4 inline-block align-text-bottom mx-2 ml-4 text-light-border relative"
+                    onMouseEnter={() => setHoverAI(true)} 
+                    onMouseLeave={() => setHoverAI(false)}
+                >
+                    AI
+                    <span 
+                        className={`absolute -left-14 top-full transition-transform duration-300 ease-in-out origin-top ${
+                            hoverAI ? 'scale-y-100' : 'scale-y-0'
+                        }`}
+                        style={{ transformOrigin: '0% 0%' }}
+                    >
+                        <span className="block whitespace-nowrap text-3xl">Artificial Intelligence</span>
+                    </span>
+                </span>
             </h2>
             <h4 className="font-sans block text-light-text sm:hidden lg:block sm:text-transparent font-base mb-4 font-base sm:font-medium leading-tight text-light-text pb-4 sm:pb-0">
                 Linkta makes learning easier by uncovering the hidden relationships between concepts.

5622cdc4db8e49382df3d7d99b9123e72cf5cee3
Andrew Lo <andrewlo957@gmail.com>
2024-05-01 00:01:34 -0700

    add hover, underline; move click outside of hyperlink text

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -18,7 +18,7 @@ export default function NotFound() {
         >
         </Player>
       </div>
-      <p className="mb-64 text-light-text">Please <a href="/" className="text-light-text underline hover:via-light-hover">click here</a> to return home or contact us at <a href="mailto:info@linkta.org">info@linkta.org</a></p>
+      <p className="mb-64 text-light-text">Please click <a href="/" className="font-bold hover:text-red-500">here</a> to return home or contact us at <a href="mailto:info@linkta.org" className="underline">info@linkta.org</a></p>
     </div>
   )
 };

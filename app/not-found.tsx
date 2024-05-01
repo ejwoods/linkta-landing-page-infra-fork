a2896f09ae30b44ef3c1560861ed092c85dda8e3
Andrew Lo <andrewlo957@gmail.com>
2024-05-01 00:09:22 -0700

    align components; adjust size of animation

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,19 +6,19 @@ import { Player } from '@lottiefiles/react-lottie-player';
 export default function NotFound() {
   
   return (
-    <div>
-      <h2 className="mt-12 text-3xl font-semibold">Oops!</h2>
-      <p className="mt-4">We couldn&apos;t find the page you&apos;re looking for</p>
-      <div aria-label='Page not found' aria-live='assertive'>
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="mt-12 text-3xl text-left font-semibold">Oops!</h2>
+      <p className="mt-4 text-left">We couldn&apos;t find the page you&apos;re looking for</p>
+      <div className="flex justify-center items-center mt-12 mb-12" aria-label='Page not found' aria-live='assertive'>
         <Player
         autoplay={true}
         loop={false}
         src={NotFoundAnimation}
-        className="w-[350px] h-[350px]"
+        className="w-[150px] h-[150px]"
         >
         </Player>
       </div>
-      <p className="mb-64 text-light-text">Please click <a href="/" className="font-bold hover:text-red-500">here</a> to return home or contact us at <a href="mailto:info@linkta.org" className="underline">info@linkta.org</a></p>
+      <p className="mb-48 text-light-text">Please click <a href="/" className="font-bold hover:text-red-500">here</a> to return home or contact us at <a href="mailto:info@linkta.org" className="underline">info@linkta.org</a></p>
     </div>
   )
 };

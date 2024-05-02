7d089a11fbbf0ad3197f338767680d0b16a3a902
ChTiSh <chentina.shen@gmail.com>
2024-05-01 23:00:01 -0700

    style thank you page and email verification prompt

--- app/components/main-content/EmailVerificationPrompt.tsx
+++ app/components/main-content/EmailVerificationPrompt.tsx
@@ -4,17 +4,15 @@ export default function EmailVerificationPrompt() {
   const linktaEmail = 'info@linkta.org';
 
   return (
-
-    <article aria-live="polite">
-    <section>
-    <p className="pt-4 text-lg">
-      Thank you for signing up on linkta.io! 
-      <br/>
-      To complete the sign-up process and verify your email address, please check your inbox on the same device you used to sign up.
-      You should have received an email from us with a verification link. If you don’t see the email in your inbox, please check your spam or junk folder. 
-      If you still can’t find it, feel free to reach out to <a href={`mailto:${linktaEmail}`}>{linktaEmail}</a> for further assistance.
-    </p>
+    <section aria-live="polite" className="pt-8 pr-6 text-light-text">
+    <h4 className="py-4 text-xl font-bold">Thank you for signing up on linkta.io!</h4> 
+      <p className="py-4 text-xl "><strong>To complete</strong> the sign-up process and verify your email address, please check your inbox on <strong>the same device </strong>you used to sign up.</p>
+      <section className="pt-2">
+        <p className="py-3">You should have received an email from us with a <strong>verification link</strong>.</p> 
+        <p>If you don’t see the email in your inbox, please check your spam or junk folder. </p>
+        <p className="pt-3">If you still can’t find it, feel free to reach out to <a href={`mailto:${linktaEmail}`} className="underline decoration-light-link font-semibold">{linktaEmail}</a> for further assistance.
+        </p>
+      </section>
     </section>
-    </article>
   )
 }
--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,12 +1,16 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import { Suspense, lazy, useEffect, useState } from 'react';
+import KeyValueProposition from '../components/main-content/KeyValueProposition';
 import { isSignInWithEmailLink } from 'firebase/auth';
 import { auth } from '../config/firebase';
 import { authenticateAndSaveUserDataFromEmailRedirect } from '../services/emailAuth';
 import { useRouter } from 'next/navigation';
 import Loading from '../loading';
 import ThankYou from '../components/main-content/ThankYou';
+import LinktaLogoWithText from '../components/layout/LinktaLogoWithText';
+
+const LandingPageTreeVisualizationPanel = lazy(() => import('../components/main-content/LandingPageTreeVisualizationPanel'));
 
 export default function ThankYouPage() {
   const [isLoading, setIsLoading] = useState(true);
@@ -27,6 +31,21 @@ export default function ThankYouPage() {
   }, [router]);
 
   return (
-    isLoading ? <Loading /> : <ThankYou />
+    <div className="lg:ml-20">
+    <span className="hidden"><LinktaLogoWithText /></span>
+    <KeyValueProposition/>
+    <div className='flex flex-col sm:flex-row sm:justify-between lg:ml-10'>
+      <Suspense fallback={<Loading />}>
+        <div className="sm:w-2/3 sm:order-2 z-0">
+          <LandingPageTreeVisualizationPanel />
+        </div>
+      </Suspense>
+      <section className="w-full flex flex-col justify-between min-w-[360px] sm:w-1/3 sm:order-1 mt-2 mb-12 xl:mx-auto">
+        <article className='max-w-[430px] z-10 -mt-4'>
+         {isLoading ? <Loading /> : <ThankYou />}
+        </article>
+      </section>
+    </div>
+  </div>
   );
 }
